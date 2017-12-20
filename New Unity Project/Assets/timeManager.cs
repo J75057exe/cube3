1b8b1fce368cff9373d76ead289797b06303bd90
J75057exe <32099438+J75057exe@users.noreply.github.com>
2017-12-20 22:02:20 +0400

    time n 6
    added new time distortion feature and prepared level 6

---
+++ New Unity Project/Assets/timeManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timeTime : MonoBehaviour
+{
+    public GameObject hint;
+
+    public Transform player;
+
+    void start()
+    {
+        
+    }
+
+    public void openUI ()
+    {
+        hint.SetActive(true);
+        Invoke("closeUI", 2);
+    }
+
+    public void closeUI()
+    {
+        hint.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Time.timeScale = 0.2f;
+            //Time.fixedDeltaTime = 0.02f * Time.timeScale;
+
+        }
+
+        if (Input.GetKey(KeyCode.Space) == false)
+        {
+            Time.timeScale = 1.0f;
+            //Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+
+        if (player.position.z > 0)
+        {
+            openUI();
+        }
+    }
+
+
+
+
+
+
+
+
+}
